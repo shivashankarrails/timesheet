fbcde9fefa361d077de1fdbd42c74b6d180f7e81
shivashankar <shivashankar@ubuntu.(none)>
2012-09-14 11:09:38 +0530

    Intial rails project app structure using rvm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timesheet::Application.config.secret_token = 'aa9d7c0eb812b484ab0f53d1551e3541dc54c71f2df5866906b5b4de2ec65b0ddba92bc019fd5a0c02cf3de4c9fb611766bc06bf16be055c0262d188c2e631dd'
